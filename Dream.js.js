718c77b3b0efb67ea2f595e9bf795cfa56640b6d
decoezz <45102309+decoezz@users.noreply.github.com>
2018-11-19 14:06:55 +0200

    Update Dream.js.js

--- Dream.js.js
+++ Dream.js.js
@@ -1,34 +1,79 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('By Wenteed')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 
-client.on('ready', async() => {
-var server = "512959538845188107"; // ايدي السررفر
-var channel = "512959538845188109";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
-})
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "lag") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
+  
+  
+  
  
-client.login('NTEzNDQzNzUyMjc5NTM5NzEy.DtINfQ.ZArz-1kCrQYtJJ6qbJNuP1CYgkw');
+  
+  
+  
+  
+  
+  });
+  
+   
+ 
+ 
+ 
+ 
+ client.on('message',function(message) {
+    let prefix = "1";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); 
+}
+});
+ 
+ 
+ 
+client.on('message', message => {
+    if (message.content === '!spam') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 90000; x++) {
+            message.channel.send(`credit credit credit credit credit credit credit credit credit credit credit credit credit credit
+credit credit credit credit credit credit credit credit credit credit credit credit credit credit
+credit credit credit credit credit credit credit credit credit credit credit credit credit credit
+credit credit credit credit credit credit credit credit credit credit credit credit credit credit
+credit credit credit credit credit credit credit credit credit credit credit credit credit credit
+credit credit credit credit credit credit credit credit credit credit credit credit credit credit ${x}`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
+    
+ 
+ 
+ 
+ 
+ 
+client.login(NTAwMDI3MzI0OTk0MTU4NjIz.DtQeqA.se-wtdCAbPgk2k2SxupPNX1sJvA);
